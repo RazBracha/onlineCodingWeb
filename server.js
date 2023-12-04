457c34d0c652bd01808c1250f82dcbe1f296f976
RazBracha <razi.bracha@gmail.com>
2023-12-04 14:10:56 +0200

    wildard

--- server.js
+++ server.js
@@ -26,7 +26,8 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://online-coding-web-client.vercel.app",
+    // origin: "https://online-coding-web-client.vercel.app",
+    origin: "*",
     methods: ["GET", "POST"],
     transports: ["websocket"],
     credentials: true,
