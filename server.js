a51206fcce25e590d42b89985aed2c897b77ead3
RazBracha <razi.bracha@gmail.com>
2023-12-06 14:49:47 +0200

    delete files

--- server.js
+++ server.js
@@ -1,34 +1,18 @@
-
-
-
 const express = require("express");
 const http = require("http");
 const cors = require("cors");
 
 const { Server } = require("socket.io");
 
-
-
 const app = express();
 app.use(cors());
+app.use(express.json())
 
 const connectDB = require('./configs/db');
 const codeBlockRouter = require('./routers/codeBlockRouter');
-const users = require('./routers/userRouter')
-
-app.use((req, res, next) => {
-  if (req.method == "OPTIONS") {
-    res.status(200);
-    return;
-  }
-  next();
-})
-
-app.use(express.json())
 
 // routers
 app.use('/codeblocks', codeBlockRouter)
-app.use('/users', users)
 
 connectDB();
 
@@ -36,7 +20,6 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    // origin: "https://online-coding-web-client.vercel.app",
     origin: "http://localhost:3000", //client
     // origin: "https://onlinecodingwebclient-production.up.railway.app",
     credentials: 'same-origin',
