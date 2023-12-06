43b5b5be3a27aa453437b74279c35f1cd0361bdd
RazBracha <razi.bracha@gmail.com>
2023-12-06 11:00:34 +0200

    cred same origin

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ const io = new Server(server, {
     // origin: "https://online-coding-web-client.vercel.app",
     // origin: "http://localhost:3000", //client
     origin: "https://onlinecodingwebclient-production.up.railway.app",
-    credentials: true,
+    credentials: 'same-origin',
   },
 });
 
