0c12ed84a77d8b83162f14e074417f652872129c
RazBracha <razi.bracha@gmail.com>
2023-12-09 12:24:24 +0200

    origin

--- server.js
+++ server.js
@@ -18,17 +18,9 @@ connectDB();
 
 const server = http.createServer(app);
 
-// const io = new Server(server, {
-//   cors: {
-//     // origin: "http://localhost:3000", //client
-//     origin: "https://onlinecodingwebclient-production.up.railway.app",
-//     credentials: 'same-origin',
-//     optionSuccessStatus: 200,
-//   },
-// });
-
 const io = new Server(server, {
   cors: {
+    // origin: "http://localhost:3000", //client
     origin: "https://onlinecodingwebclient-production.up.railway.app",
     methods: ["GET", "POST"],
     credentials: true, // Allow credentials (cookies)
